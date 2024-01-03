7f30cced56e9d3a212cbbc5f62a76c877620824b
Md. Mahbubul Alam <152076580+mahbublabib9@users.noreply.github.com>
2024-01-03 16:55:58 +0600

    Add files via upload

---
+++ Selection sort.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+void swap(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+void selectionSort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+    int minIndex = i;
+    for (int j = i + 1; j < n; j++) {
+        if (arr[j] < arr[minIndex]) {
+            minIndex = j;
+        }
+    }
+        if (minIndex != i) {
+        swap(&arr[i], &arr[minIndex]);
+    }
+}
+}
+void printArray(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        printf("%d ", arr[i]);
+}
+    printf("\n");
+}
+int main() {
+    int arr[] = {90, 80, 30, 70, 20, 10};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    printf("Original array: ");
+    printArray(arr, n);
+    selectionSort(arr, n);
+    printf("Sorted array in ascending order: ");
+    printArray(arr, n);
+    return 0;
+}
+
